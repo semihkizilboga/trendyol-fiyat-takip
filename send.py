343f6fc5b48e98db8ece2fe3ac1f90ebcf1396e9
Semih Kızılboğa <78806541+semihkizilboga@users.noreply.github.com>
2021-08-30 00:00:03 +0300

    e-posta işlemi

--- send.py
+++ send.py
@@ -1,5 +1,27 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def sendMail(toMail, subject, content):
 
-    fromMail = "grafiksemih@gmail.com"
+    fromMail = "xxxxx@gmail.com"
+    server = smtplib.SMTP("smtp.gmail.com",587)
+
+    server.starttls()
     
+    server.login(fromMail, "XXXXXXXX")
+
+    message = MIMEMultipart('alternative')
+    message['Subject'] = subject
+
+    htmlContent = MIMEText(content, 'html')
+    message.attach(htmlContent)
+
+    server.sendmail(
+        fromMail,
+        toMail,
+        message.as_string()
+    )
+    print("E-posta gönderildi.")
+
+    server.quit()
